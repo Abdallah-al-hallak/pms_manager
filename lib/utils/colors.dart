0d236bb213515350cfaee4cecbc2d597e679b136
Abdallah al hallak <bode.1996.bh.bh@gmail.com>
2023-03-22 21:17:19 +0200

    first

---
+++ lib/utils/colors.dart
@@ -0,0 +1,4 @@
+import 'package:flutter/material.dart';
+
+const Color gold = Color.fromRGBO(191, 168, 134, 1);
+const Color lightDart = Color.fromRGBO(35, 35, 35, 1);
---
+++ lib/utils/styles.dart
@@ -0,0 +1,5 @@
+import 'package:flutter/material.dart';
+
+TextStyle standardTextStyle() {
+  return const TextStyle(fontSize: 18, letterSpacing: 0.6, color: Colors.white);
+}
