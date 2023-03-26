a79996e17689ceb12c65b3b92fcc482064d433bf
ruaaghali <ruaagh5656@outlook.com>
2023-03-26 13:51:18 +0300

    Add the sign in button and make some changes

--- lib/features/intro/widgets/my_textfield.dart
+++ lib/features/intro/widgets/my_textfield.dart
@@ -15,28 +15,25 @@ class MyTextField extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: SizedBox(
-        // width: ,
-        height: 50,
-        child: TextField(
-          controller: controller,
-          obscureText: obscureText,
-          decoration: InputDecoration(
-              enabledBorder: OutlineInputBorder(
-                borderSide: const BorderSide(color: gold),
-                borderRadius: BorderRadius.circular(9.0),
-              ),
-              focusedBorder: OutlineInputBorder(
-                borderRadius: BorderRadius.circular(9.0),
-                borderSide: const BorderSide(color: gold),
-              ),
-              fillColor: gold,
-              filled: true,
-              hintText: hintText,
-              hintStyle: TextStyle(color: Colors.white.withOpacity(0.4))),
-        ),
+    return SizedBox(
+      // width: ,
+      height: 50,
+      child: TextField(
+        controller: controller,
+        obscureText: obscureText,
+        decoration: InputDecoration(
+            enabledBorder: OutlineInputBorder(
+              borderSide: const BorderSide(color: gold),
+              borderRadius: BorderRadius.circular(9.0),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(9.0),
+              borderSide: const BorderSide(color: gold),
+            ),
+            fillColor: gold,
+            filled: true,
+            hintText: hintText,
+            hintStyle: TextStyle(color: Colors.white.withOpacity(0.4))),
       ),
     );
   }
