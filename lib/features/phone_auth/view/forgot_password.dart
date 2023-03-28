5b604af24ce3645b00d7ee5ae1c4c32eb8161509
ruaaghali <ruaagh5656@outlook.com>
2023-03-28 23:28:58 +0300

    add forget password feature

---
+++ lib/features/phone_auth/view/forgot_password.dart
@@ -0,0 +1,75 @@
+import 'package:flutter/material.dart';
+import 'package:pms_manager/features/intro/widgets/custom_buton.dart';
+import 'package:pms_manager/features/intro/widgets/my_textfield.dart';
+import 'package:pms_manager/features/phone_auth/view/verify_number.dart';
+
+
+class ForgotPassword extends StatelessWidget {
+  ForgotPassword({super.key});
+
+  // text editing controllers
+  final phoneNumberController = TextEditingController();
+
+  // sign user in method
+  void signUserIn() {}
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return Scaffold(
+      backgroundColor: Colors.grey[300],
+      body: SingleChildScrollView(
+        child: LayoutBuilder (
+            builder: (context, p1)=>
+                SafeArea(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Center(
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          const SizedBox(height: 50),
+
+                          // logo
+                          Image.asset(
+                            "assets/png/logo.png",
+                            fit: BoxFit.contain,
+                          ),
+
+                          const SizedBox(height: 50),
+
+                          Padding(
+                            padding: const EdgeInsets.only(left: 25, right: 25),
+                            child: MyTextField(
+                              controller: phoneNumberController,
+                              hintText: 'Phone Number',
+                              obscureText: false,
+                            ),
+                          ),
+
+                          const SizedBox(height: 25),
+
+                          Padding(
+                            padding: const EdgeInsets.only(left: 25, right: 25),
+                            child: CustomElevatedButton(
+                              onPressed: () {
+                                Navigator.pushReplacement(context, MaterialPageRoute(
+                                  builder: (context) {
+                                    return VerifyNumber();
+                                  },
+                                ));
+                              },
+                              text: 'Send Code',
+                              height: 50,
+                            ),
+                          ),
+                        ],
+                      ),
+                    ),
+                  ),
+                )
+        ),
+      ),
+    );
+  }
+}
