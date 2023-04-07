557ee794145ec010044f53259a710e82aa299690
Abdallah al hallak <bode.1996.bh.bh@gmail.com>
2023-04-07 19:51:01 +0300

    .

--- lib/features/home/screens/lease_details.dart
+++ lib/features/home/screens/lease_details.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
+import 'package:intl/intl.dart';
 import 'package:pms_manager/features/home/view/home_page.dart';
+import 'package:pms_manager/features/intro/widgets/custom_buton.dart';
 import 'package:pms_manager/utils/colors.dart';
+import 'package:pms_manager/utils/styles.dart';
 
 final leaseSidebarWidgetsProvider = StateProvider<LeaseSidebarWidgets>((ref) {
   return LeaseSidebarWidgets.contract;
@@ -48,11 +51,11 @@ extension BodyWidgetExtension on LeaseSidebarWidgets {
   Widget get displayWidget {
     switch (this) {
       case LeaseSidebarWidgets.contract:
-        return const Center(child: Text('contract'));
+        return const ContractWidget();
       case LeaseSidebarWidgets.tenant:
-        return const Center(child: Text('tenant'));
+        return const TenantWidget();
       case LeaseSidebarWidgets.parking:
-        return const Text('parking');
+        return const ParkingWidget();
       case LeaseSidebarWidgets.expenses:
         return const Text('expenses');
       case LeaseSidebarWidgets.reminders:
@@ -63,24 +66,311 @@ extension BodyWidgetExtension on LeaseSidebarWidgets {
   }
 }
 
-extension SideWidgetColorExtension on LeaseSidebarWidgets {
-  Color get displayColor {
-    switch (this) {
-      case LeaseSidebarWidgets.contract:
-        return Colors.white;
-      case LeaseSidebarWidgets.tenant:
-        return Colors.white;
-      case LeaseSidebarWidgets.parking:
-        return Colors.white;
-      case LeaseSidebarWidgets.expenses:
-        return Colors.white;
-      case LeaseSidebarWidgets.reminders:
-        return Colors.white;
-      case LeaseSidebarWidgets.attachments:
-        return Colors.white;
-      default:
-        return Colors.black;
-    }
+class LeaseFieldsTemplate extends ConsumerWidget {
+  const LeaseFieldsTemplate({super.key, required this.widget, this.height});
+  final Widget widget;
+  final double? height;
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    var size = MediaQuery.of(context).size;
+    return SizedBox(
+      width: size.width,
+      height: height ?? size.width / 5.5,
+      child: Card(
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(10)),
+        elevation: 3,
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 10),
+          child: widget,
+        ),
+      ),
+    );
+  }
+}
+
+class ContractWidget extends ConsumerStatefulWidget {
+  const ContractWidget({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ContractWidgetState();
+}
+
+class _ContractWidgetState extends ConsumerState<ContractWidget> {
+  var date1 = DateTime.now().toUtc();
+  var date2 = DateTime.now().toUtc();
+  change(DateTime time) {
+    setState(() {
+      date1 = time;
+    });
+  }
+
+  change2(DateTime time) {
+    setState(() {
+      date2 = time;
+    });
+  }
+
+  yMD(DateTime time) {
+    String formatted = DateFormat('yMd').format(time);
+    return formatted;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                selectDat(context, DateTime.now(), change);
+              },
+              child: LeaseFieldsTemplate(
+                widget: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Start Date',
+                      style: gooldTextStyle(),
+                    ),
+                    SizedBox(width: 0.0, height: 5.0),
+                    Text('${yMD(date1)}')
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                selectDat(context, DateTime.now(), change);
+              },
+              child: LeaseFieldsTemplate(
+                widget: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'End Date',
+                      style: gooldTextStyle(),
+                    ),
+                    SizedBox(width: 0.0, height: 5.0),
+                    Text('${yMD(date2)}')
+                  ],
+                ),
+              ),
+            ),
+            LeaseFieldsTemplate(
+              widget: GeneralDropDown(
+                width: size.width / 1.6,
+                list: const ['Furnishing', 'abd', 'ruaa'],
+                value: 'Furnishing',
+              ),
+            ),
+            LeaseFieldsTemplate(
+              widget: GeneralDropDown(
+                width: size.width / 1.6,
+                list: const ['Payment Frequency', 'abd', 'ruaa'],
+                value: 'Payment Frequency',
+              ),
+            ),
+            const LeaseFieldsTemplate(
+                widget: FilterTextFields(
+              hint: 'Electricity number',
+              trype: TextInputType.number,
+            )),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Rent Amount',
+                  style: gooldTextStyle(),
+                ),
+                const SizedBox(width: 30.0, height: 0.0),
+                SizedBox(
+                  height: 50,
+                  width: size.width / 3.5,
+                  child: Card(
+                    child: Center(
+                        child: Text(
+                      '8,000 AED',
+                      style: gooldTextStyle(),
+                    )),
+                  ),
+                ),
+              ],
+            ),
+            const SizedBox(width: 0.0, height: 20.0),
+            CustomElevatedButton(text: 'Add contract', onPressed: () {}),
+            const SizedBox(width: 0.0, height: 10.0),
+            CustomElevatedButton(text: 'Next', onPressed: () {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ParkingWidget extends ConsumerStatefulWidget {
+  const ParkingWidget({super.key});
+
+  @override
+  ConsumerState<ConsumerStatefulWidget> createState() => _ParkingWidgetState();
+}
+
+class _ParkingWidgetState extends ConsumerState<ParkingWidget> {
+  var date1 = DateTime.now().toUtc();
+  var date2 = DateTime.now().toUtc();
+  change(DateTime time) {
+    setState(() {
+      date1 = time;
+    });
+  }
+
+  change2(DateTime time) {
+    setState(() {
+      date2 = time;
+    });
+  }
+
+  yMD(DateTime time) {
+    String formatted = DateFormat('yMd').format(time);
+    return formatted;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var size = MediaQuery.of(context).size;
+    return SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.all(12),
+        child: Column(
+          children: [
+            GestureDetector(
+              onTap: () {
+                selectDat(context, DateTime.now(), change);
+              },
+              child: LeaseFieldsTemplate(
+                widget: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'Start Date',
+                      style: gooldTextStyle(),
+                    ),
+                    const SizedBox(width: 0.0, height: 5.0),
+                    Text('${yMD(date1)}')
+                  ],
+                ),
+              ),
+            ),
+            GestureDetector(
+              onTap: () {
+                selectDat(context, DateTime.now(), change);
+              },
+              child: LeaseFieldsTemplate(
+                widget: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      'End Date',
+                      style: gooldTextStyle(),
+                    ),
+                    const SizedBox(width: 0.0, height: 5.0),
+                    Text('${yMD(date2)}')
+                  ],
+                ),
+              ),
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  'Rent Amount',
+                  style: gooldTextStyle(),
+                ),
+                const SizedBox(width: 30.0, height: 0.0),
+                SizedBox(
+                  height: 50,
+                  width: size.width / 3.5,
+                  child: Card(
+                    child: Center(
+                        child: Text(
+                      '8,000 AED',
+                      style: gooldTextStyle(),
+                    )),
+                  ),
+                ),
+              ],
+            ),
+            LeaseFieldsTemplate(
+                height: size.width / 2,
+                widget: const FilterTextFields(
+                  hint: 'Notes',
+                )),
+            const SizedBox(width: 0.0, height: 20.0),
+            CustomElevatedButton(
+                text: 'Add Another Parking Spot', onPressed: () {}),
+            const SizedBox(width: 0.0, height: 10.0),
+            CustomElevatedButton(
+                text: 'Continue Without Parking Spot', onPressed: () {}),
+            const SizedBox(width: 0.0, height: 10.0),
+            CustomElevatedButton(text: 'Next', onPressed: () {}),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class TenantWidget extends ConsumerWidget {
+  const TenantWidget({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Padding(
+      padding: const EdgeInsets.all(12),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Tenant Phone',
+                style: TextStyle(fontSize: 17),
+              ),
+              Text('............')
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Tenant Email',
+                style: TextStyle(fontSize: 17),
+              ),
+              Text('............')
+            ],
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: const [
+              Text(
+                'Tenant Attachments',
+                style: TextStyle(fontSize: 17),
+              ),
+              Text('............')
+            ],
+          ),
+          const SizedBox(width: 0.0, height: 250),
+          CustomElevatedButton(text: 'Next', onPressed: () {}),
+        ],
+      ),
+    );
   }
 }
 
@@ -88,10 +378,10 @@ class BodyWidget extends ConsumerStatefulWidget {
   const BodyWidget({super.key});
 
   @override
-  ConsumerState<ConsumerStatefulWidget> createState() => _BosyWidgetState();
+  ConsumerState<ConsumerStatefulWidget> createState() => _BodyWidgetState();
 }
 
-class _BosyWidgetState extends ConsumerState<BodyWidget> {
+class _BodyWidgetState extends ConsumerState<BodyWidget> {
   @override
   Widget build(BuildContext context) {
     return Container(
@@ -124,28 +414,31 @@ class _BarWidgetState extends ConsumerState<BarWidget> {
         mainAxisAlignment: MainAxisAlignment.spaceEvenly,
         children: [
           GestureDetector(
-              onTap: () {
-                ref.read(leaseSidebarWidgetsProvider.notifier).state =
-                    LeaseSidebarWidgets.contract;
-              },
-              child: Image.asset(
-                'assets/png/contract.png',
-                color: lease == LeaseSidebarWidgets.contract
-                    ? Colors.white
-                    : lightDark,
-                scale: 0.8,
-              )),
+            onTap: () {
+              ref.read(leaseSidebarWidgetsProvider.notifier).state =
+                  LeaseSidebarWidgets.contract;
+            },
+            child: Image.asset(
+              'assets/png/contract.png',
+              color: lease == LeaseSidebarWidgets.contract
+                  ? Colors.white
+                  : lightDark,
+              scale: 0.8,
+            ),
+          ),
+          const Text('Contract'),
           GestureDetector(
-              onTap: () {
-                ref.read(leaseSidebarWidgetsProvider.notifier).state =
-                    LeaseSidebarWidgets.tenant;
-              },
-              child: Image.asset(
-                'assets/png/fg.png',
-                color: lease == LeaseSidebarWidgets.tenant
-                    ? Colors.white
-                    : lightDark,
-              )),
+            onTap: () {
+              ref.read(leaseSidebarWidgetsProvider.notifier).state =
+                  LeaseSidebarWidgets.tenant;
+            },
+            child: Image.asset(
+              'assets/png/fg.png',
+              color: lease == LeaseSidebarWidgets.tenant
+                  ? Colors.white
+                  : lightDark,
+            ),
+          ),
           GestureDetector(
               onTap: () {
                 ref.read(leaseSidebarWidgetsProvider.notifier).state =
@@ -157,6 +450,7 @@ class _BarWidgetState extends ConsumerState<BarWidget> {
                     ? Colors.white
                     : lightDark,
               )),
+          const Text('Parkinng'),
           GestureDetector(
               onTap: () {
                 ref.read(leaseSidebarWidgetsProvider.notifier).state =
@@ -168,6 +462,7 @@ class _BarWidgetState extends ConsumerState<BarWidget> {
                     ? Colors.white
                     : lightDark,
               )),
+          const Text('Expenses'),
           GestureDetector(
               onTap: () {
                 ref.read(leaseSidebarWidgetsProvider.notifier).state =
@@ -179,6 +474,7 @@ class _BarWidgetState extends ConsumerState<BarWidget> {
                     ? Colors.white
                     : lightDark,
               )),
+          const Text('Reminders'),
           GestureDetector(
               onTap: () {
                 ref.read(leaseSidebarWidgetsProvider.notifier).state =
@@ -190,6 +486,7 @@ class _BarWidgetState extends ConsumerState<BarWidget> {
                     ? Colors.white
                     : lightDark,
               )),
+          const Text('Attahment'),
         ],
       ),
     );
